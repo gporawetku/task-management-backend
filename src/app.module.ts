98ce4ecc2546dd372b0528fd34b8e52c5bb3e97e
Porawet Ku <gporawet.ku@gmail.com>
2024-08-04 01:08:21 +0700

    add service and dto user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Task } from './tasks/entities/task.entity';
+import { Project } from './projects/entities/project.entity';
 
 @Module({
-  imports: [UsersModule, TasksModule, ProjectsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'task_management_database',
+      entities: [User, Task, Project],
+      synchronize: true, // Set to false in production
+    }),
+    UsersModule,
+    TasksModule,
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
